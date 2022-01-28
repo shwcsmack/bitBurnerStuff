f58b4ebacd38a8791125f16174529c59fad89122
shwcsmack <sh.wc.smack@gmail.com>
2022-01-27 21:42:47 -0600

    added loop

--- basic_hack.js
+++ basic_hack.js
@@ -9,21 +9,31 @@ export async function main(ns) {
 
 	ns.tprint(`Hacking server: ${server}`)
 
-	if (currentSecurityLevel > minSecurityLevel) {
-		ns.tprint(`Current Security Level: ${currentSecurityLevel}, Min: ${minSecurityLevel}`)
-		let timeToWeaken = ns.getWeakenTime(server);
-		ns.tprint(`Weakening. This will take ${ns.tFormat(timeToWeaken)}`)
-		await ns.weaken(server)
-	} else if (currentMoneyAvailible < maxMoney) {
-		ns.tprint(`Current Money Availible: ${currentMoneyAvailible}, Max: ${maxMoney}`)
-		let timeToGrow = ns.tFormat(ns.getGrowTime(server));
-		ns.tprint("Growing. This will take " + timeToGrow);
-		await ns.grow(server)
-	} else {
-		let timeToHack = ns.tFormat(ns.timeToHack(server))
-		ns.tprint("Hacking. This will take " + timeToHack)
-		let earnedMoney = await ns.hack(server)
-		ns.tprint(`Stole \$${earnedMoney.toLocaleString()}`)
+	if (!ns.hasRootAccess(server)) {
+		ns.tprint("Getting root access")
+		if (ns.getServerNumPortsRequired(server) > 0) {
+			ns.brutessh(server);
+		}
+		ns.nuke(server);
 	}
 
+	while(true) {
+		if (currentSecurityLevel > minSecurityLevel) {
+			ns.tprint(`Current Security Level: ${currentSecurityLevel}, Min: ${minSecurityLevel}`)
+			let timeToWeaken = ns.getWeakenTime(server);
+			ns.tprint(`Weakening. This will take ${ns.tFormat(timeToWeaken)}`)
+			await ns.weaken(server)
+		} else if (currentMoneyAvailible < maxMoney) {
+			ns.tprint(`Current Money Availible: ${currentMoneyAvailible}, Max: ${maxMoney}`)
+			let timeToGrow = ns.tFormat(ns.getGrowTime(server));
+			ns.tprint("Growing. This will take " + timeToGrow);
+			await ns.grow(server)
+		} else {
+			let timeToHack = ns.tFormat(ns.timeToHack(server))
+			ns.tprint("Hacking. This will take " + timeToHack)
+			let earnedMoney = await ns.hack(server)
+			ns.tprint(`Stole \$${earnedMoney.toLocaleString()}`)
+		}
+	}
+	
 }
