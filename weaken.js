2d320528c16ef7e4b6a1a094abdda32adbea3c36
shwcsmack <sh.wc.smack@gmail.com>
2022-01-29 16:42:45 -0600

    Create weaken.js

---
+++ weaken.js
@@ -0,0 +1,11 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+
+	if (ns.args.length == 2) {
+		const sleeptime = ns.args[1];
+		await ns.sleep(sleeptime);
+	}
+	
+	const server = ns.args[0];
+	await ns.weaken(server);
+}
